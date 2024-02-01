341e180bd985946c59285f378d4e4e5448e84117
Uddhav Pisharody <101915996+uddhav-pisharody@users.noreply.github.com>
2024-02-01 10:56:35 +0530

    Add files via upload

---
+++ Density Based Algorithms/DBSCAN/DBSCAN.py
@@ -0,0 +1,116 @@
+import numpy as np
+
+def dbscan(D, eps, MinPts):
+    '''
+    Cluster the dataset `D` using the DBSCAN algorithm.
+    
+    dbscan takes a dataset `D` (a list of vectors), a threshold distance
+    `eps`, and a required number of points `MinPts`.
+    
+    It will return a list of cluster labels. The label -1 means noise, and then
+    the clusters are numbered starting from 1.
+    '''
+   
+    labels = [0]*len(D)
+    
+    C = 0
+    
+    for P in range(0, len(D)):
+    
+        if not (labels[P] == 0):
+           continue
+        
+        NeighborPts = region_query(D, P, eps)
+        
+        if len(NeighborPts) < MinPts:
+            labels[P] = -1
+           
+        else: 
+           C += 1
+           grow_cluster(D, labels, P, NeighborPts, C, eps, MinPts)
+    
+    return labels
+
+
+def grow_cluster(D, labels, P, NeighborPts, C, eps, MinPts):
+    '''
+    Grow a new cluster with label `C` from the seed point `P`.
+    
+    This function searches through the dataset to find all points that belong
+    to this new cluster. When this function returns, cluster `C` is complete.
+    
+    Parameters:
+      `D`      - The dataset (a list of vectors)
+      `labels` - List storing the cluster labels for all dataset points
+      `P`      - Index of the seed point for this new cluster
+      `NeighborPts` - All of the neighbors of `P`
+      `C`      - The label for this new cluster.  
+      `eps`    - Threshold distance
+      `MinPts` - Minimum required number of neighbors
+    '''
+
+    # Assign the cluster label to the seed point.
+    labels[P] = C
+    
+    # Look at each neighbor of P (neighbors are referred to as Pn). 
+    # NeighborPts will be used as a FIFO queue of points to search--that is, it
+    # will grow as we discover new branch points for the cluster. The FIFO
+    # behavior is accomplished by using a while-loop rather than a for-loop.
+    # In NeighborPts, the points are represented by their index in the original
+    # dataset.
+    i = 0
+    while i < len(NeighborPts):    
+        
+        # Get the next point from the queue.        
+        Pn = NeighborPts[i]
+       
+        # If Pn was labelled NOISE during the seed search, then we
+        # know it's not a branch point (it doesn't have enough neighbors), so
+        # make it a leaf point of cluster C and move on.
+        if labels[Pn] == -1:
+           labels[Pn] = C
+        
+        # Otherwise, if Pn isn't already claimed, claim it as part of C.
+        elif labels[Pn] == 0:
+            # Add Pn to cluster C (Assign cluster label C).
+            labels[Pn] = C
+            
+            # Find all the neighbors of Pn
+            PnNeighborPts = region_query(D, Pn, eps)
+            
+            # If Pn has at least MinPts neighbors, it's a branch point!
+            # Add all of its neighbors to the FIFO queue to be searched. 
+            if len(PnNeighborPts) >= MinPts:
+                NeighborPts = NeighborPts + PnNeighborPts
+            # If Pn *doesn't* have enough neighbors, then it's a leaf point.
+            # Don't queue up it's neighbors as expansion points.
+            #else:
+                # Do nothing                
+                #NeighborPts = NeighborPts               
+        
+        # Advance to the next point in the FIFO queue.
+        i += 1        
+    
+    # We've finished growing cluster C!
+
+
+def region_query(D, P, eps):
+    '''
+    Find all points in dataset `D` within distance `eps` of point `P`.
+    
+    This function calculates the distance between a point P and every other 
+    point in the dataset, and then returns only those points which are within a
+    threshold distance `eps`.
+    '''
+    neighbors = []
+    
+    D_array = np.array(D)
+    
+    # For each point in the dataset...
+    for Pn in range(0, len(D)):
+        
+        # If the distance is below the threshold, add it to the neighbors list.
+        if np.linalg.norm(D_array[P] - D_array[Pn]) < eps:
+           neighbors.append(Pn)
+            
+    return neighbors
